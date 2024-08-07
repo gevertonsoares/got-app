8554b3b3d4a5f4cc4b265f52fd4343e5bb6f5572
Geverton Soares <gevertonsoares22@gmail.com>
2024-08-07 19:29:57 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { AppRoutes } from "./config/routes/AppRoutes";
+import { GlobalStyles } from "./config/routes/global/GlobalStyles";
+
+
+
+export default function App() {
+  
+
+  return (
+    <>
+      <GlobalStyles />
+      <AppRoutes />
+    </>
+  )
+}
+
+
